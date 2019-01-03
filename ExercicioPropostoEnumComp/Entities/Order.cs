eb3a90cdbc72b2f3e1428744821fd37cd0dcb88f
caiovicto546 <caiovicto@gmail.com>
2019-01-03 18:20:00 -0200

    Inclusão de Classes Client, Order, OrderItem, Product e Enumerados OrderStatus

---
+++ ExercicioPropostoEnumComp/Entities/Order.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExercicioPropostoEnumComp.Entities.Enums;
+
+namespace ExercicioPropostoEnumComp.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+
+        public Order()
+        {
+        }
+
+        public Order(Client client, OrderStatus status)
+        {
+            this.Client = client;
+            this.Moment = DateTime.Now;
+            this.Status = status;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            
+            
+        }
+
+
+    }
+}
---
+++ ExercicioPropostoEnumComp/Entities/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPropostoEnumComp.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get;set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity)
+        {
+            this.Quantity = quantity;
+            this.Price = Product.Price;
+        }
+
+        public double SubTotal()
+        {
+            return Price * Quantity;
+        }
+    }
+}
